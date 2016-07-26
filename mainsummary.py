8f49e54ccabce3a3cab1ab457be16ccc501c6c8f
hessamoddin <hessamoddin.shafeian@gmail.com>
2016-07-26 19:41:49 -0400

    Update mainsummary.py

--- mainsummary.py
+++ mainsummary.py
@@ -4,10 +4,11 @@ import scipy.io as matreader
 # Read the summary file
 loaded_summary=matreader.loadmat('/home/hessam/code/data/GT/Air_Force_One.mat')
 nFrames=loaded_summary['gt_score'].shape[0]
-summary_score=nFrames=loaded_summary['gt_score']
+summary_score=loaded_summary['gt_score']
 
 #Subsample the video
 starting_frame=1
 ending_frame=nFrames
-timestep=80
-selected_keyframes=[starting_frame:ending_frame:timestep]
+step=80  #sampling step
+time_steps=10  #number of LSTMs per video
+selected_keyframes=summary_score[starting_frame:ending_frame:timestep]
