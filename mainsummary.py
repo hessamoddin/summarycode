4ef8ac06a923d4a2117ff35c315c7e8ba11d9545
hessamoddin <hessamoddin.shafeian@gmail.com>
2016-07-31 19:23:26 -0400

    Update mainsummary.py

--- mainsummary.py
+++ mainsummary.py
@@ -1,3 +1,4 @@
+############ Load Libraries ##############
 from __future__ import division, print_function, absolute_import
 import numpy as np
 import scipy.io as matreader
@@ -9,6 +10,22 @@ import pylab
 import imageio
 from skimage.transform import resize
 from skimage.color import rgb2gray
+from skimage.feature import daisy
+import matplotlib.cbook as cbook
+
+############ Extract frame features ##############
+def Daisy_Extractor_Fn(vid,frame_no,new_shape=(120,180),step=50, radius=20):
+    if frame_no<num_frames: 
+        frame = vid.get_data(frame_no)  
+        frame_resized=resize(frame, new_shape)
+        frame_gray= rgb2gray(frame_resized)
+        daisy_desc = daisy(frame_gray,step=step, radius=radius)
+        descs_1D=np.ravel(daisy_desc)
+    else:
+        print("Frame number is larger than the length of video")
+    return descs_1D
+    
+
 
 
 # To split video into different evenly sized set of frames to feed into LSTMs
@@ -18,14 +35,13 @@ def chunks(l, n):
         yield l[i:i+n]
         
         
-# Load this video file
+############ Load Video ##############
 videofilename='/home/hessam/code/data/videos/Air_Force_One.mp4'
 vid = imageio.get_reader(videofilename,  'ffmpeg')
 # number of frames in video
 num_frames=vid._meta['nframes']
 
-######## Extract frame features ##############
-
+############ Extract frame features ##############
 #Subsample the video
 starting_frame=1
 ending_frame=num_frames
@@ -35,22 +51,12 @@ sampling_id=np.arange(starting_frame,ending_frame,step)
 video_sequence_frameid=list(chunks(sampling_id, num_LSTMs)) #batch of video sequence of frame ids
 batch_size=len(video_sequence_frameid)  # batch size: number of rows of sequential data to be fed to LSTMs
 
+for i in xrange(batch_size):
+    for j in xrange(num_LSTMs):
+       current_frame_id=video_sequence_frameid[i][j]
+       current_feature=Daisy_Extractor_Fn(vid,current_frame_id)
 
-######## Load Video Clip  Raw Data ##############
-
-#10th frame--should be changed
-frame = vid.get_data(10) 
-#resize frame
-frame_resized=resize(frame, (100, 100))
-#convert the color frame to gray-scale
-frame_gray= rgb2gray(frame_resized)
-
-
-
-
-
-
-# Read the summary file
+############ Load Summary File ##############
 data_path='/home/hessam/code/data/GT'
 onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
 num_videos=len(onlyfiles)
@@ -65,8 +71,4 @@ summary_score=loaded_summary['gt_score']
 # LSTM input size: Batch size (num sequesnces/rows) [if None, it can be changed] X 
 #   Sequence length X Dimension of each member of sequence
 
-
-
-# Iterate over keyframes of all segments of videos
-for i in range(0,num_splitted_video):
-  print(i)
+ 
