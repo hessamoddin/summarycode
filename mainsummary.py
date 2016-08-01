a44b08c1245564e9f34e1f6071dbaea340fefb72
hessamoddin <hessamoddin.shafeian@gmail.com>
2016-07-31 21:43:25 -0400

    Update mainsummary.py

--- mainsummary.py
+++ mainsummary.py
@@ -69,7 +69,7 @@ def dictionary(descriptors, N):
 	#save("weights.gmm", gmm.weights_)
 	return float32(gmm.means_),float32(gmm.covars_),float32(gmm.weights_)
 
-
+a,b,c= Feature_Extractor_Fn(vid,frame_no)
 for i in xrange(batch_size):
     for j in xrange(num_LSTMs):
        current_frame_id=video_sequence_frameid[i][j]
