56c91dd12dbd4aa7139fe01280be088e41178e39
hessamoddin <hessamoddin.shafeian@gmail.com>
2016-08-02 17:27:52 -0400

    Update mainsummary.py

--- mainsummary.py
+++ mainsummary.py
@@ -2,6 +2,7 @@
 ###  Summarization algorithm by Hessamoddin Shafeian
 ############ Load Libraries ##############
 from __future__ import division, print_function, absolute_import
+import sklearn.mixture.gmm as gm
 import numpy as np
 import scipy.io as matreader
 import pprint
@@ -16,6 +17,97 @@ from skimage.color import rgb2gray
 from skimage.feature import daisy
 import matplotlib.cbook as cbook
 from sklearn.cluster import KMeans
+
+# Reference: https://github.com/rkwitt/pyfsa/blob/master/core/fsa.py
+
+def estimate_gm(X,components=3,seed=None):
+    """Estimate a Gaussian mixture model.
+    Note: Uses diagonal covariance matrices.
+    Parameters
+    ----------
+    X : numpy matrix, shape (N,D)
+        Matrix of data samples (i-th row is i-th sample vector).
+    c : int (default : 3)
+        Number of desired mixture components.
+    seed : int (default : None)
+        Seed for the random number generator.
+    Returns
+    -------
+    gm_obj : sklearn.mixture.gmm object
+        Estimated GMM.
+    """
+
+    logger = logging.getLogger()
+
+    n, d = X.shape
+    logger.info("Estimating %d-comp. GMM from (%d x %d) ..." %
+                (components, n, d))
+
+    gm_obj = gm.GMM (n_components=components,
+                     covariance_type='diag',
+                     random_state=seed)
+
+    gm_obj.fit(X)
+    return gm_obj
+
+
+def learn_codebook(X, codebook_size=200, seed=None):
+    """Learn a codebook.
+    Run K-Means clustering to compute a codebook. K-Means
+    is initialized by K-Means++, uses a max. of 500 iter-
+    ations and 10 times re-initialization.
+    Paramters
+    ---------
+    X : numpy matrix, shape (N,D)
+        Input data.
+    codebook_size : int (default : 200)
+        Desired number of codewords.
+    seed : int (default : None)
+        Seed for random number generator.
+    Returns
+    -------
+    cb : sklearn.cluster.KMeans object
+        KMeans object after fitting.
+    """
+
+    logger = logging.getLogger()
+    logger.info("Learning codebook with %d words ..." % codebook_size)
+
+    # Run vector-quantization
+    cb = KMeans(codebook_size,
+                init="k-means++",
+                n_init=10,
+                max_iter=500,
+                random_state=seed)
+    cb.fit(X)
+    return cb
+
+
+def bow(X, cb):
+    """Compute a (normalized) BoW histogram.
+    Parameters
+    ----------
+    X : numpy matrix, shape (N, D)
+        Input data.
+    cb : sklearn.cluster.KMeans
+        Already estimated codebook with C codewords.
+    Returns
+    -------
+    H : numpy array, shape (C,)
+        Normalized (l2-norm) BoW histogram.
+    """
+
+    # Get nr. codewords
+    n,d = cb.cluster_centers_.shape
+    if d != X.shape[1]:
+        raise Exception("Dimensionality mismatch!")
+    # Compute closest cluster centers
+    assignments = cb.predict(X)
+    # Compute (normalized) BoW histogram
+    B = range(0,n+1)
+    return np.histogram(assignments,bins=B,density=True)[0]
+
+
 
 ############ Extract frame features ##############
 def Feature_Extractor_Fn(vid,frame_no,new_shape=(120,180),step=50, radius=20):
