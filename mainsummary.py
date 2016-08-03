e70f26aac938d5ae9b2fcf64567e69ece77c98c8
hessamoddin <hessamoddin.shafeian@gmail.com>
2016-08-02 20:15:26 -0400

    Update mainsummary.py

--- mainsummary.py
+++ mainsummary.py
@@ -3,6 +3,7 @@
 ############ Load Libraries ##############
 from __future__ import division, print_function, absolute_import
 import sklearn.mixture.gmm as gm
+from sklearn import mixture
 import numpy as np
 import scipy.io as matreader
 import pprint
@@ -49,9 +50,19 @@ def estimate_gm(X,components=1000,seed=None):
                      covariance_type='diag',
                      random_state=seed)
 
-    gm_obj.fit(X)
+    gm_obj.fit(X)   
+  #  np.random.seed(1)
+   # g = mixture.GMM(n_components=components)
+#    g.fit(X)
+ #   np.round(g.weights_, 2)    
+  #  np.round(g.means_, 2)
+   # np.round(g.covars_, 2) 
+    #g.predict(X)
+#    np.round(g.score(X), 2)
+
     return gm_obj
 
+# # Reference for GMM and Kmeans with a bit of modification: https://github.com/rkwitt/pyfsa/blob/master/core/fsa.py
 # Choose default codebook size and keep it the same for both GMM and K-means method of encoding
 def learn_codebook(X, codebook_size=1000, seed=None):
     """Learn a codebook.
@@ -84,7 +95,7 @@ def learn_codebook(X, codebook_size=1000, seed=None):
     cb.fit(X)
     return cb
 
-
+# # Reference for GMM and Kmeans : https://github.com/rkwitt/pyfsa/blob/master/core/fsa.py
 def bow(X, cb):
     """Compute a (normalized) BoW histogram.
     Parameters
@@ -109,7 +120,66 @@ def bow(X, cb):
     B = range(0,n+1)
     return np.histogram(assignments,bins=B,density=True)[0]
 
+############ Extract frame features ##############
+# https://github.com/jacobgil/pyfishervector/blob/master/fisher.py
 
+def dictionary(descriptors, N):
+    em = cv2.ml.EM_create()
+    em.setClustersNumber(N)
+    em.trainEM(descriptors)
+    means = em.getMeans()
+    covs = em.getCovs()
+    weights = em.getWeights()
+    return np.float32(means), np.float32(covs), np.float32(weights)
+    
+def likelihood_moment(x, ytk, moment):	
+	x_moment = np.power(np.float32(x), moment) if moment > 0 else np.float32([1])
+	return x_moment * ytk
+	
+def likelihood_statistics(samples, means, covs, weights):
+	gaussians, s0, s1,s2 = {}, {}, {}, {}
+	samples = zip(range(0, len(samples)), samples)
+	
+	g = [multivariate_normal(mean=means[k], cov=covs[k]) for k in range(0, len(weights)) ]
+	for index, x in samples:
+		gaussians[index] = np.array([g_k.pdf(x) for g_k in g])
+
+	for k in range(0, len(weights)):
+		s0[k], s1[k], s2[k] = 0, 0, 0
+		for index, x in samples:
+			probabilities = np.multiply(gaussians[index], weights)
+			probabilities = probabilities / np.sum(probabilities)
+			s0[k] = s0[k] + likelihood_moment(x, probabilities[k], 0)
+			s1[k] = s1[k] + likelihood_moment(x, probabilities[k], 1)
+			s2[k] = s2[k] + likelihood_moment(x, probabilities[k], 2)
+
+	return s0, s1, s2
+
+def fisher_vector_weights(s0, s1, s2, means, covs, w, T):
+	return np.float32([((s0[k] - T * w[k]) / np.sqrt(w[k]) ) for k in range(0, len(w))])
+
+def fisher_vector_means(s0, s1, s2, means, sigma, w, T):
+	return np.float32([(s1[k] - means[k] * s0[k]) / (np.sqrt(w[k] * sigma[k])) for k in range(0, len(w))])
+
+def fisher_vector_sigma(s0, s1, s2, means, sigma, w, T):
+	return np.float32([(s2[k] - 2 * means[k]*s1[k]  + (means[k]*means[k] - sigma[k]) * s0[k]) / (np.sqrt(2*w[k])*sigma[k])  for k in range(0, len(w))])
+
+def normalize(fisher_vector):
+	v = np.sqrt(abs(fisher_vector)) * np.sign(fisher_vector)
+	return v / np.sqrt(np.dot(v, v))
+
+def fisher_vector(samples, means, covs, w):
+	s0, s1, s2 =  likelihood_statistics(samples, means, covs, w)
+	T = samples.shape[0]
+	covs = np.float32([np.diagonal(covs[k]) for k in range(0, covs.shape[0])])
+	a = fisher_vector_weights(s0, s1, s2, means, covs, w, T)
+	b = fisher_vector_means(s0, s1, s2, means, covs, w, T)
+	c = fisher_vector_sigma(s0, s1, s2, means, covs, w, T)
+	fv = np.concatenate([np.concatenate(a), np.concatenate(b), np.concatenate(c)])
+	fv = normalize(fv)
+	return fv
+
+	
 
 ############ Extract frame features ##############
 def Feature_Extractor_Fn(vid,frame_no,new_shape=(120,180),step=50, radius=20):
